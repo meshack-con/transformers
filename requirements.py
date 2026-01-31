f7be4e904f0c58ba8a91874bc6d7795c0db4e9d8
meshack-con <kondowemeshack591@gmail.com>
2026-01-31 13:54:09 +0300

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,25 +1,6 @@
-import subprocess
-import os
-
-# 1. Orodha ya maktaba tulizotumia
-requirements = """streamlit
 psutil
+streamlit-mic-recorder
+SpeechRecognition
+gTTS
 pyautogui
 Pillow
-"""
-
-file_name = "requirements.txt"
-
-# 2. Tengeneza na andika faili hilo
-with open(file_name, "w") as f:
-    f.write(requirements)
-
-print(f"Faili la {file_name} limetengenezwa!")
-
-# 3. Fungua Notepad kuonyesha faili hilo (Kwa Windows pekee)
-try:
-    # Hii amri inafungua Notepad na faili letu
-    subprocess.Popen(['notepad.exe', file_name])
-    print("Notepad imefunguliwa.")
-except Exception as e:
-    print(f"Imeshindwa kufungua Notepad: {e}")
